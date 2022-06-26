7a71311fbe590782ab7f1977672d9185b92bd2ba
Denis Shubin <denis.shubin@gmail.com>
2022-06-26 15:38:57 +0300

    feat: filtering parameters in URL

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import {Categories} from '../components/Categories';
-import {Sort} from '../components/Sort';
+import {Sort, sortList} from '../components/Sort';
 import {Skeleton} from '../components/PizzaBlock/Skeleton';
 import {itemsType, PizzaBlock} from '../components/PizzaBlock/PizzaBlock';
 import {Pagination} from '../components/Pagination/Pagination';
 import {SearchContext} from '../App';
 import {useDispatch, useSelector} from 'react-redux';
-import {FilterState, setCategoryId, setCurrentPage} from '../redux/slices/filterSlice';
+import {FilterState, setCategoryId, setCurrentPage, setFilters} from '../redux/slices/filterSlice';
 import {RootState} from '../redux/store';
 import axios from 'axios';
+import qs from 'qs';
+import {useNavigate} from 'react-router-dom';
 
 export const Home = () => {
+    const navigate = useNavigate()
     const dispatch = useDispatch()
+    const isSearch = React.useRef(false)
+    const isMounted = React.useRef(false)
+
     const {categoryId, sort, currentPage} = useSelector<RootState, FilterState>((state) => state.filter)
 
     const {searchValue} = React.useContext(SearchContext)
@@ -23,11 +29,7 @@ export const Home = () => {
         dispatch(setCategoryId(id))
     }
 
-    const onChangePage = (number: number) => {
-        dispatch(setCurrentPage(number))
-    }
-
-    React.useEffect(() => {
+    const fetchPizzas = () => {
         setIsLoading(true)
 
         const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
@@ -41,7 +43,44 @@ export const Home = () => {
                 setItems(res.data)
                 setIsLoading(false)
             })
+    }
+
+    // Если изменили параметры и был первый рендер
+    React.useEffect(() => {
+        if (isMounted.current) {
+            const queryString = qs.stringify({
+                sortProperty: sort.sortProperty,
+                categoryId,
+                currentPage,
+            })
+            navigate(`?${queryString}`)
+        }
+        isMounted.current = true
+    }, [categoryId, sort.sortProperty, searchValue, currentPage])
+
+    // Если был первый рендер, то проверяем URL-параметры и сохраняем в Redux
+    React.useEffect(() => {
+        if (window.location.search) {
+            const params = qs.parse(window.location.search.substring(1))
+            const sort = sortList.find(obj => obj.sortProperty === params.sortProperty)
+            dispatch(setFilters({
+                ...params, sort
+            }))
+        }
+        isSearch.current = true
+    }, [])
+
+    const onChangePage = (number: number) => {
+        dispatch(setCurrentPage(number))
+    }
+
+    // Если был первый рендер, то запрашиваем пиццы
+    React.useEffect(() => {
         window.scrollTo(0, 0)
+        if (!isSearch.current) {
+            fetchPizzas()
+        }
+        isSearch.current = false
     }, [categoryId, sort.sortProperty, searchValue, currentPage])
 
     const pizzas = items.map((obj) => <PizzaBlock
